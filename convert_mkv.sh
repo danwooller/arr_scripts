261f77e1e718152453bf1778342d019e38f16570
Dan Wooller <dan@wooller.com>
2026-02-13 13:19:59 +0000

    Update convert_mkv.sh

--- convert_mkv.sh
+++ convert_mkv.sh
@@ -81,6 +81,7 @@ while true; do
             mkvmerge -o "${OUTPUT_FILE}" "${FULL_PATH}" > /dev/null 2>&1
             
             if [ $? -eq 0 ]; then
+               log "✅ Successfully converted ${FULL_FILE_NAME}""
                 if [[ $LOG_LEVEL = "debug" ]]; then
                     log "Success. Moving original to finished."
                 fi
