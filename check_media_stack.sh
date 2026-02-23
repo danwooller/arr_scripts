066db2c69d04c5bb1a3f4b2ee6f751ff619c9019
Dan Wooller <dan@wooller.com>
2026-02-23 17:23:40 +0000

    Update check_media_stack.sh

--- check_media_stack.sh
+++ check_media_stack.sh
@@ -41,6 +41,9 @@ echo "--- Media Stack Connectivity Diagnostic ---"
 # Bazarr (Subtitles)
 check_service "Bazarr ($BAZARR_API_VER)" "$BAZARR_API_BASE" "$BAZARR_API_KEY" "/api/$BAZARR_API_VER/system/status"
 
+# Check Dispatcharr (Checking if the API documentation/spec is reachable)
+check_service "Dispatcharr" "$DISPATCHARR_API_BASE" "NONE" "/openapi.json"
+
 # Lidarr (Music)
 check_service "Lidarr ($LIDARR_API_VER)" "$LIDARR_API_BASE" "$LIDARR_API_KEY" "/system/status"
 
