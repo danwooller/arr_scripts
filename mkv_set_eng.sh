f895d3c36773fb9580d7153f64ac366f43991f1b
Dan Wooller <dan@wooller.com>
2026-02-25 13:10:04 +0000

    Update mkv_set_eng.sh

--- mkv_set_eng.sh
+++ mkv_set_eng.sh
@@ -6,6 +6,7 @@ source "/usr/local/bin/common_functions.sh"
 # Configuration
 SOURCE_DIR="${1:-/mnt/media/Movies}"
 DRY_RUN=${DRY_RUN:-false}        # Defaults to false if not set/exported
+#LOG_LEVEL="debug"
 LOG_LEVEL=${LOG_LEVEL:-"info"}   # Defaults to info if not set
 
 # Reset the internal timer
