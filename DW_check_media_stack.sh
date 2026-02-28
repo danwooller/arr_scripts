500cbe4b29261c2b5cfc3107b0b83fcdb827f255
Dan Wooller <dan@wooller.com>
2026-02-28 18:41:48 +0000

    Update DW_check_media_stack.sh

--- DW_check_media_stack.sh
+++ DW_check_media_stack.sh
@@ -120,13 +120,13 @@ done
 
 # 4. Config File
 #CONFIG_FILE="/mnt/media/torrent/ubuntu9_sonarr.txt"
-#echo -n "Checking Config File... "
-#if [[ -f "$CONFIG_FILE" ]]; then
-#    echo -e "${GREEN}FOUND${NC}"
-#else
-#    echo -e "${RED}NOT FOUND ($CONFIG_FILE)${NC}"
-#    ((TOTAL_ERRORS++))
-#fi
+echo -n "Checking Config File... "
+if [[ -f "$CONFIG_FILE" ]]; then
+    echo -e "${GREEN}FOUND${NC}"
+else
+    echo -e "${RED}NOT FOUND ($CONFIG_FILE)${NC}"
+    ((TOTAL_ERRORS++))
+fi
 
 echo "-------------------------------------------"
 if [ $TOTAL_ERRORS -eq 0 ]; then
