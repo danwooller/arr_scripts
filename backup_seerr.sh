08516b28bae8bebcf9cf90b29ee2a772070e2c68
danwooller <danwooller@gmail.com>
2026-02-28 12:49:44 +0000

    Update backup_seerr.sh from ubuntu24

--- backup_seerr.sh
+++ backup_seerr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # --- Setup & Includes ---
-source "/usr/local/bin/common_functions.sh"
+source "/usr/local/bin/DW_common_functions.sh"
 
 # --- Variables ---
 DB_CONTAINER="seerr-db"
