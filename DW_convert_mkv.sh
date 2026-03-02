a78d6169ce6dbfb9363d840472a1ee266d7dcaa3
Dan Wooller <dan@wooller.com>
2026-03-02 14:04:43 +0000

    Update DW_convert_mkv.sh

--- DW_convert_mkv.sh
+++ DW_convert_mkv.sh
@@ -24,7 +24,8 @@ mkdir -p "$OUTPUT_DIR" "$EXT_VIDEO"
 # --- Run Dependency Check using the shared function ---
 check_dependencies "HandBrakeCLI" "jq" "mkvmerge" "mkvpropedit" "rename"
 
-#log_start "$WATCH_DIRS"
+log_start "$WATCH_DIRS"
+log_start
 
 while true; do
     for TARGET_DIR in "${WATCH_DIRS[@]}"; do
