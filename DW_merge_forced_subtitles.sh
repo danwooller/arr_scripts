a8d3a1a06b38a0464e3891f240ea5faaa844bb4e
Dan Wooller <dan@wooller.com>
2026-03-04 16:31:16 +0000

    Update DW_merge_forced_subtitles.sh

--- DW_merge_forced_subtitles.sh
+++ DW_merge_forced_subtitles.sh
@@ -5,7 +5,13 @@
 # Use for FORCED subtitles.
 
 # --- Load Shared Functions ---
-source "/usr/local/bin/DW_common_functions.sh"
+# Checking existence to prevent 'set -e' from killing the script cryptically
+if [ -f "/usr/local/bin/DW_common_functions.sh" ]; then
+    source "/usr/local/bin/DW_common_functions.sh"
+else
+    echo "⚠️ /usr/local/bin/DW_common_functions.sh missing. Exiting."
+    exit 1
+fi
 
 # --- Configuration ---
 HOST=$(hostname -s)
