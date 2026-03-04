b6993f0262a084770cd568edec9768052d2b2fe2
Dan Wooller <dan@wooller.com>
2026-03-04 16:32:00 +0000

    Update DW_mkv_tv_shows_title.sh

--- DW_mkv_tv_shows_title.sh
+++ DW_mkv_tv_shows_title.sh
@@ -5,7 +5,13 @@
 # the name of the immediate containing folder.
 
 # --- Load Shared Functions ---
-source "/usr/local/bin/DW_common_functions.sh"
+# Checking existence to prevent 'set -e' from killing the script cryptically
+if [ -f "/usr/local/bin/DW_common_functions.sh" ]; then
+    source "/usr/local/bin/DW_common_functions.sh"
+else
+    echo "⚠️ /usr/local/bin/DW_common_functions.sh missing. Exiting."
+    exit 1
+fi
 
 # --- Dependencies ---
 check_dependencies "mkvmerge"
