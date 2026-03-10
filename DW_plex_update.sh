c0d7cd1d64f0e87d2374ec92fd9cea15b5702bf2
Dan Wooller <dan@wooller.com>
2026-03-10 13:43:03 +0000

    Create DW_plex_update.sh

---
+++ DW_plex_update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# --- Load Shared Functions ---
+if [ -f "/usr/local/bin/DW_common_functions.sh" ]; then
+    source "/usr/local/bin/DW_common_functions.sh"
+else
+    echo "⚠️ Common functions missing."
+    exit 1
+fi
