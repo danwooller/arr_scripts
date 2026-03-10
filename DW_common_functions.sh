82c94bd1b635488e9ef7738a445b0cc5427c236c
Dan Wooller <dan@wooller.com>
2026-03-10 16:35:33 +0000

    Update DW_common_functions.sh

--- DW_common_functions.sh
+++ DW_common_functions.sh
@@ -205,9 +205,8 @@ notify_sonarr_targeted_rename() {
 
     # Fetch Series ID
     local series_id=$(curl -s -H "X-Api-Key: $SONARR_API_KEY" "$SONARR_API_BASE/series" | \
-        jq -r ".[] | select(.path | endswith(\"/$show_name\")) | .id")
-log "curl -s -H  X-Api-Key: $SONARR_API_KEY  $SONARR_API_BASE/series  | \
-        jq -r  .[] | select(.path | endswith(\ /$show_name\)) | .id"
+        jq -r ".[] | select(.path | contains(\"/$show_name\")) | .id")
+
     if [ -n "$series_id" ] && [ "$series_id" != "null" ]; then
         
         # 1. Trigger Refresh (Disk Scan)
