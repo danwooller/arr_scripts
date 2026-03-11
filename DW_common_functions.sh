68333a14b17c06eb3f4a33864e17f923ddad52f7
Dan Wooller <dan@wooller.com>
2026-03-11 21:07:17 +0000

    Update DW_common_functions.sh

--- DW_common_functions.sh
+++ DW_common_functions.sh
@@ -319,6 +319,13 @@ plex_library_update() {
     # DW_move_movies_synology.sh
     # DW_move_tv_shows_synology.sh
     # DW_sort_tv.sh
+
+#TEMP
+    if ! nc -z -w 2 192.168.1.50 32400; then
+        log "❌ Plex server port 32400 is not reachable. Skipping scan."
+        return 1
+    fi
+
     local section_id="$1"
     local library_name="$2"
     local url="$PLEX_URL"
