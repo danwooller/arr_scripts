b06dd1899cd6adadb89ae1993dbf2c479110b944
Dan Wooller <dan@wooller.com>
2026-03-11 12:57:42 +0000

    Update git_pull_install.sh

--- git_pull_install.sh
+++ git_pull_install.sh
@@ -52,12 +52,10 @@ if [ -f "$DEST_DIR/DW_common_seerr_issue.sh" ]; then
     sudo chmod +x "$BIN_DIR/DW_common_seerr_issue.sh"
     sudo chown root:root "$BIN_DIR/DW_common_seerr_issue.sh"
 fi
-if [ -f "$DEST_DIR/common_keys.txt" ]; then
-    echo "Updating $COMMON_KEYS..."
-    sudo cp "$COMMON_KEYS/common_keys.txt" "$BIN_DIR/"
-    sudo chmod +x "$BIN_DIR/common_keys.txt"
-    sudo chown root:root "$BIN_DIR/common_keys.txt"
-fi
+echo "Updating $COMMON_KEYS..."
+sudo cp "$COMMON_KEYS/common_keys.txt" "$BIN_DIR/"
+sudo chmod +x "$BIN_DIR/common_keys.txt"
+sudo chown root:root "$BIN_DIR/common_keys.txt"
 
 if [ -n "$FILENAME" ]; then
     # 4. Sync script to system bin
