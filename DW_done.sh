ffb160606d71793825e8b565a9946cc540dd1986
Dan Wooller <dan@wooller.com>
2026-03-18 17:02:09 +0000

    Update DW_done.sh

--- DW_done.sh
+++ DW_done.sh
@@ -34,7 +34,7 @@ while true; do
         # Truncate name to fit (78 - 4 padding - $MAX_FILES size - 2 gap = 64)
         [ ${#name} -gt 64 ] && name="${name:0:61}..."
         printf "│   %-8s %-65s │\n" "$size" "$name"
-        pad_len=$((INNER - ${#size} - ${#name}))
+        pad_len=$(( $INNER - ${#size} - ${#name} ))
         printf "│%s%${pad_len}s│\n" "$size" "$name"
     done
     if [ "$count" -gt "$MAX_FILES" ]; then
