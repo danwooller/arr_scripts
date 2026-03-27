5e41f1f8464ad8aed3f189e8daca0c4618334f21
Dan Wooller <dan@wooller.com>
2026-03-27 15:52:40 +0000

    Update DW_monitor_convert.sh

--- DW_monitor_convert.sh
+++ DW_monitor_convert.sh
@@ -72,6 +72,7 @@ while true; do
                         log "$name" "tv" "Merge failed"
                         seerr_sync_issue "$name" "tv" "Merge failed for $FILENAME"
                         log "❌ Merge failed for $FILENAME"
+                        mv "$FILENAME" "DIR_MEDIA_HOLD/"
                     fi
                     #[[ $LOG_LEVEL == "debug" ]] && log "Moving: $(basename "$file")"
                     # The trailing slash ensures it treats the destination as a directory
