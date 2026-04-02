9a516f4c92da9c457eba4ecf8c2f00a4cce584e1
Dan Wooller <dan@wooller.com>
2026-04-02 11:19:02 +0100

    Update DW_monitor_convert.sh

--- DW_monitor_convert.sh
+++ DW_monitor_convert.sh
@@ -124,7 +124,7 @@ while true; do
         else
             PRESET="$PRESET_SD"
         fi
-log "$HANDBRAKE_SUB_ARGS"
+log "sub file: $SUB_FILE"
         [[ $LOG_LEVEL == "debug" ]] && log "ℹ️ Using preset: $PRESET"
         HandBrakeCLI \
             --preset "$PRESET" \
