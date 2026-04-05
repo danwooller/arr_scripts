a4abf5cae5bc60b18e598c193fb57827c197768c
Dan Wooller <dan@wooller.com>
2026-04-05 17:08:48 +0100

    Update DW_done_colour.sh

--- DW_done_colour.sh
+++ DW_done_colour.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# --- Load Shared Functions ---
+if [ -f "/usr/local/bin/DW_common_functions.sh" ]; then
+    source "/usr/local/bin/DW_common_functions.sh"
+else
+    echo "⚠️ /usr/local/bin/DW_common_functions.sh missing. Exiting."
+    exit 1
+fi
+
 # 1. Setup Variables
 WIDTH=${1:-80}
 INNER=$((WIDTH - 2))
