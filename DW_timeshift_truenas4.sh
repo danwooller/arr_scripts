23280659f4e250285bff5359ab9c18b48b0a9a03
Dan Wooller <dan@wooller.com>
2026-06-15 10:13:56 +0100

    Update DW_timeshift_truenas4.sh

--- DW_timeshift_truenas4.sh
+++ DW_timeshift_truenas4.sh
@@ -12,7 +12,8 @@ fi
 # --- CONFIGURATION ---
 SSD_DEVICE="/dev/sda"
 SSD_MOUNT="/mnt/ssd_recovery"
-NAS_PATH="/mnt/tank1/backup/$HOST"
+#NAS_PATH="/mnt/tank1/backup/$HOST"
+NAS_PATH="/mnt/tank1/backup"
 MOUNT_POINT="/mnt/truenas4_backup"
 SSD_SNAPSHOTS="$SSD_MOUNT/timeshift/snapshots/"
 trap 'sudo umount -l $MOUNT_POINT $SSD_MOUNT 2>/dev/null' EXIT
@@ -35,7 +36,7 @@ echo "Mounting TrueNAS..."
 sudo mount -t nfs -o soft,timeo=50,retrans=2 $BASE_HOST4:$NAS_PATH $MOUNT_POINT
 
 if mountpoint -q "$MOUNT_POINT" && mountpoint -q "$SSD_MOUNT"; then
-    sudo mkdir -p $NAS_PATH
+    sudo mkdir -p $NAS_PATH/$HOST
     [[ "$LOG_LEVEL" == "debug" ]] && log "ℹ️ Drives ready, starting sync..."
     
     # --timeout=180: If no data moves for 3 mins (NAS went to sleep), rsync kills itself
